e602a413b3a1aef9099857c02275a2b77002172c
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2023-02-08 11:01:02 -0500

    fix mobile nav

--- components/NavFive.vue
+++ components/NavFive.vue
@@ -64,67 +64,28 @@
       <!-- content is loading via js -->
 
       <ul class="main-nav__navigation-box">
-        <li class="dropdown current">
-          <div class="menu-holder">Home<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
-          <ul>
-            <li><nuxt-link to="/">Home 01</nuxt-link></li>
-            <li><nuxt-link to="/index2">Home 02</nuxt-link></li>
-            <li><nuxt-link to="/index3">Home 03</nuxt-link></li>
-            <li><nuxt-link to="/index4">Home 04</nuxt-link></li>
-            <li><nuxt-link to="/index5">Home 05</nuxt-link></li>
-            <li class="dropdown">
-              <div class="menu-holder">Header Versions<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
-              <ul>
-                <li><nuxt-link to="/">Header 01</nuxt-link></li>
-                <li><nuxt-link to="/index2">Header 02</nuxt-link></li>
-                <li><nuxt-link to="/index3">Header 03</nuxt-link></li>
-                <li><nuxt-link to="/index4">Header 04</nuxt-link></li>
-                <li><nuxt-link to="/index5">Header 05</nuxt-link></li>
-              </ul><!-- /.sub-menu -->
-            </li>
-          </ul><!-- /.sub-menu -->
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/aboutus">About</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/parksAlliance">Our Friends</nuxt-link>
         </li>
         <li class="dropdown">
-          <div class="menu-holder">Services<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
+          <div class="menu-holder">Our Boats<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
           <ul>
-            <li><nuxt-link to="/service">Services</nuxt-link></li>
-            <li><nuxt-link to="/service-detail">Services Detail</nuxt-link></li>
-          </ul><!-- /.sub-menu -->
-        </li>
-        <li class="dropdown">
-          <div class="menu-holder">Our Projects<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
-          <ul>
-            <li><nuxt-link to="/projects">Projects</nuxt-link></li>
-            <li><nuxt-link to="/projects_detail">Projects Detail</nuxt-link></li>
-          </ul><!-- /.sub-menu -->
-        </li>
-        <li class="dropdown">
-          <div class="menu-holder">Shop<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
-          <ul>
-            <li><nuxt-link to="/product">Products</nuxt-link></li>
-            <li><nuxt-link to="/product-detail">Product Detail</nuxt-link></li>
-            <li><nuxt-link to="/cart">Cart</nuxt-link></li>
-            <li><nuxt-link to="/checkout">Checkout</nuxt-link></li>
-          </ul><!-- /.sub-menu -->
-        </li>
-        <li class="dropdown">
-          <div class="menu-holder">Pages<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
-          <ul>
-            <li><nuxt-link to="/about">About</nuxt-link></li>
-            <li><nuxt-link to="/why_choose_us">Why Choose Us</nuxt-link></li>
-            <li><nuxt-link to="/farmers">Farmers</nuxt-link></li>
-            <li><nuxt-link to="/gallery">Gallery</nuxt-link></li>
-          </ul><!-- /.sub-menu -->
-        </li>
-        <li class="dropdown">
-          <div class="menu-holder">News<button class="dropdown-btn"><i class="fa fa-angle-right"></i></button></div>
-          <ul>
-            <li><nuxt-link to="/news">News</nuxt-link></li>
-            <li><nuxt-link to="/news_detail">News Details</nuxt-link></li>
+            <li><nuxt-link to="/canoes">Canoes</nuxt-link></li>
+            <li><nuxt-link to="/kayaks">Kayaks</nuxt-link></li>
+            <li><nuxt-link to="/paddleboards">Stand-Up Paddleboards</nuxt-link></li>
           </ul><!-- /.sub-menu -->
         </li>
         <li>
-          <nuxt-link to="/contact">Contact</nuxt-link>
+          <nuxt-link to="/shop">Shop</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/contact">Contact Us</nuxt-link>
         </li>
       </ul>
     </nav>
